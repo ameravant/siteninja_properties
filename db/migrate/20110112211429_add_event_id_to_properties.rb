096780197d27a040a1b35755bbc1b0e1f81ef8df
Ameravant <contact@ameravant.com>
2011-07-07 13:19:59 -0700

    removing event migration

--- db/migrate/20110112211429_add_event_id_to_properties.rb
+++
@@ -1,9 +0,0 @@
-class AddEventIdToProperties < ActiveRecord::Migration
-  def self.up
-    add_column :events, :property_id, :integer
-  end
-
-  def self.down
-    remove_column :events, :property_id
-  end
-end
