2981d5fff8c0ed6a6c6efccb8f2fedc7a1f5e7ca
Dave Myers <contact@ameravant.com>
2012-01-13 14:25:19 -0800

    modifications to image model

--- lib/image.rb
+++ lib/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
   require "#{Rails.root}/vendor/plugins/siteninja/siteninja_core/app/models/image"
-  has_attached_file :image, {:styles => { :icon => "32x16>", :thumb => "48x36#", :square => "75x75#", :small => "85x55#", :medium_thumb => "156x120#", :large_thumb => "176x145#", :medium => "200x9999>", :large => "880x9999>", :slide => "550x9999>", :wide_slide => "870x9999>", :side_column => "278x9999>"}}.merge(PAPERCLIP_OPS)
+  has_attached_file :image, {:styles => { :icon => "32x16>", :thumb => "48x36#", :square => "75x75#", :small => "85x55#", :medium_thumb => "156x120#", :large_thumb => "176x145#", :medium => "200x9999>", :large => "880x9999>", :slide => "#{!CMS_CONFIG['site_settings']['feature_box_width'].blank? ? CMS_CONFIG['site_settings']['feature_box_width'] : '550'}x9999>", :wide_slide => "#{!CMS_CONFIG['site_settings']['wide_feature_box_width'].blank? ? CMS_CONFIG['site_settings']['wide_feature_box_width'] : '870'}x9999>" }}.merge(PAPERCLIP_OPS)
   validates_presence_of :title, :on => :create
 end
